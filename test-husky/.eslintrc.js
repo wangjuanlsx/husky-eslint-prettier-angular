3998a50ed8d5da79a7f892629a1ab876da45da83
wangjuan <wangjuan@travelsky.com>
2020-02-16 16:22:46 +0800

    提交

---
+++ test-husky/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["eslint-config-ali","prettier", "plugin:prettier/recommended"],
+  "parser": "babel-eslint",
+  "rules": {
+    "prettier/prettier": "error",
+    "strict": "off",
+    "no-console": "error",
+    "import/no-dynamic-require": "off",
+    "global-require": "off",
+    "require-yield": "off",
+  },
+  "plugins": ["prettier"],
+};
